bbf02f1fb8dfdabe983e4c3f6f97df0bca482694
GabrielbFranca <gabrielbfranca@gmail.com>
2021-08-30 12:05:51 -0300

    Add project files.

---
+++ Classe/conta.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_dio.Classe
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private string Nome { get; set; }
+        private double Saldo { get; set; }
+        public double Credito { get; set; }
+
+
+        public Conta(TipoConta TipoConta, double Saldo, double Credito, string Nome)
+        {
+            this.TipoConta = TipoConta;
+            this.Saldo = Saldo;
+            this.Nome = Nome;
+            this.Credito = Credito;
+        }
+        public bool Sacar(double valorSaldo)
+        {
+            if (Saldo - valorSaldo < (Credito * -1))
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+            Saldo -= valorSaldo;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+
+            return true;
+
+        }
+        public void Depositar(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+        }
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Crédito " + this.Credito;
+            return retorno;
+
+        }
+    }
+}
